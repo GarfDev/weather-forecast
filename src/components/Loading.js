50ed078bd77afc3683c9fbeb5b959dabe0ad5468
GarfDev <garfdev@gmail.com>
2019-10-16 23:58:09 +0700

    Rocket 2

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -10,8 +10,8 @@ export default function Loading(props) {
   async function getTodayData(latitude, longitude) {
     const API_KEY = "380dddc3f0ec41ca8c9d1c038397b245";
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-    let response = await fetch(url);
-    let data = await response.json();
+    let response = await fetch(url).catch(error => alert(error));
+    let data = await response.json().catch(error => alert(error));
     props.changeTodayData(data);
     console.log(data);
     props.getLoading(false);
@@ -20,9 +20,9 @@ export default function Loading(props) {
   async function getWeatherData(latitude, longitude) {
     const API_KEY = "380dddc3f0ec41ca8c9d1c038397b245";
     const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&cnt=45`;
-    let response = await fetch(url);
+    let response = await fetch(url).catch(error => alert(error));
     let dates = [];
-    let data = await response.json();
+    let data = await response.json().catch(error => alert(error));
     let cleaneData = data.list.filter(card => {
       let Datez = new Date(card.dt * 1000);
       let Day = Datez.getDay();
